0062e8f0f5ee5a606c910e21f6e692f10262f082
Dan Njoroge <greatdaniels@gmail.com>
2020-02-16 22:30:15 +0300

    getting form data and storing it in variables

---
+++ js/akan.js
@@ -0,0 +1,18 @@
+function akan(){
+    var day = document.getElementById("day").value;
+    var DD = parseInt(day);
+    var year = document.getElementById("year").value;
+    var year1 = year.toString();
+    var year2 = year1.slice(2,4);
+
+    var YY = parseInt(year2);
+
+    var month = document.getElementById("month").value;
+    var MM = parseInt(month);
+     
+}
+
+
+
+
+
